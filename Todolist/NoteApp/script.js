9dc164e7097e2a9391f95a4689ca7a94a644fc0d
Athulya <athulyaa175@gmail.com>
2025-08-06 22:03:11 +0530

    cloaning

---
+++ Todolist/NoteApp/script.js
@@ -0,0 +1,30 @@
+
+function addNotes(){
+    console.log("clk")
+    const notes=document.createElement("div")
+    notes.id="note-id"
+    notes.innerHTML=`
+    <div class="container">
+        <div class="contnotes">
+            <textarea placeholder="type here......"></textarea>
+            <img src="/icons/trash.png" class="trashicon">
+        </div>
+    </div>
+
+    `
+    const trash=notes.querySelector(".trashicon")
+    trash.addEventListener("click",function(){
+        const container=trash.closest(".container")
+        if(container){
+            container.remove()
+        }
+        })
+
+    document.body.appendChild(notes)
+}
+
+const addNote=document.querySelector(".createnotebtn")
+addNote.addEventListener("click",addNotes)
+
+
+
