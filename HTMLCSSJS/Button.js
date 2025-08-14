b44df227e44f5ecf9ea2187a55d756d0a19ef4ac
Athulya <athulyaa175@gmail.com>
2025-08-14 14:51:07 +0530

    questions

--- HTMLCSSJS/Button.js
+++ HTMLCSSJS/Button.js
@@ -1,3 +1,5 @@
+//*****************************Beginner Level*************************** */
+
 /*
     //Problem-1
     let container =document.querySelector(".container")
@@ -48,6 +50,7 @@
 */
 
 /*
+    //problem4
     const hov=document.getElementById("hov")
     hov.addEventListener("mouseover",()=>{
         hov.textContent="Leave me"
@@ -59,18 +62,196 @@
     })
 */
 
-
+//***********************Intermediate Level******************************** */
 /*
-    const text=document.querySelector(".txt")
+    //problem5
+    const text= document.querySelector(".txt")
     const btn=document.querySelector(".btn")
-    const list=document.getElementById("unorderd")
+    const unorderd=document.getElementById("unorderd")
     btn.addEventListener("click",()=>{
-        const texts=text.value
-        if(texts!==""){
+        const lists=text.value
+        if(lists!==""){
             const li=document.createElement("li")
-            li.textContent=texts
-            list.appendChild(li)
+            li.textContent=lists
+            unorderd.appendChild(li)
             text.value=""
         }
     })
 */
+
+
+/*
+    //problem6
+    const color=document.querySelector(".color")
+    const click=document.querySelector(".click")
+    const Showclr=document.querySelector(".Showclr")
+    click.addEventListener("click",()=>{
+        const filclr=color.value
+        Showclr.style.backgroundColor=`${filclr}`
+        color.textContent=""
+    })
+*/
+
+/*
+    //problem7
+    const input=document.querySelector(".input")
+    const click=document.querySelector(".click")
+    const unorderd=document.getElementById("unorderd")
+    click.addEventListener("click",()=>{
+        let contents=input.value
+        let li=document.createElement("li")
+        li.classList.add("listid")
+        li.textContent=contents
+
+        let cross=document.createElement("span")
+        cross.innerHTML="\u00d7"
+        li.append(cross)
+        unorderd.appendChild(li)
+        input.value=""
+        cross.addEventListener("click",(event)=>{
+                li.remove()
+            })
+        li.addEventListener("click",()=>{
+            li.style.textDecoration="line-through"
+            li.style.listStyle="disc"
+        
+        })
+    })
+*/
+
+
+/*
+    //problem8
+    const alldt=document.getElementsByTagName("dt")
+    const alldd=document.getElementsByTagName("dd")
+    for(let i=0;i<alldt.length;i++){
+        alldt[i].addEventListener("click",()=>{
+            if(alldd[i].style.display=="block"){
+                alldd[i].style.display="none"
+            }
+            else{
+                alldd[i].style.display="block"
+            }
+        
+    })
+    }
+*/
+
+
+/*
+    //problem9
+    const images=document.querySelectorAll(".problem9 img")
+    const prev=document.getElementById("prev")
+    const next=document.getElementById("next")
+    let curindx=0
+    images.forEach(x=>x.style.display="none")
+    images[curindx].style.display="block"
+
+    next.addEventListener("click",()=>{
+        images[curindx].style.display="none"
+        curindx=(curindx+1+images.length)%images.length
+        images[curindx].style.display="block"
+    })
+    prev.addEventListener("click",()=>{
+        images[curindx].style.display="none"
+        curindx=(curindx-1+images.length)%images.length
+        images[curindx].style.display="block"
+    })
+*/
+
+//****************************Challenge Level******************************* */
+
+/*
+const labels=document.getElementsByTagName("input")
+const btn=document.querySelector(".submit")
+const form=document.getElementById("form")
+const names=document.getElementById("name")
+const password=document.getElementById("password")
+const email=document.getElementById("email")
+form.addEventListener("submit",function(event){
+    names.setCustomValidity("")
+    password.setCustomValidity("")
+    email.setCustomValidity("")
+    if(names.value.trim()===""){
+        names.setCustomValidity("Please enter your name")
+    }
+    if(!email.value.includes("@")){
+        email.setCustomValidity("Email must contain '@'")
+    }
+    if(password.value.length<6){
+        password.setCustomValidity("Password must be atleast 6 characters")
+    }else{
+        password.setCustomValidity("")
+    }
+   if(!form.checkValidity()){
+    form.reportValidity()
+     event.preventDefault()
+   }
+})
+*/
+// btn.addEventListener("click",()=>{
+//     console.log("click")
+//     for(let i=0;i<labels.length;i++){
+//         if(labels[i].value==""){
+//         alert("please fill all fields")
+//         return
+//     }else{
+//         alert("form submitted Succesfully Refresh the page")
+//         return
+//     }
+    
+//     }
+// })
+
+
+/*
+    //promblem 10
+    const names=document.getElementById("name")
+    const password=document.getElementById("password")
+    const email=document.getElementById("email")
+    const form=document.getElementById("form")
+    form.addEventListener("submit",()=>{
+        names.setCustomValidity("")
+        password.setCustomValidity("")
+        email.setCustomValidity("")
+        if(names.value.trim()==""){
+            names.setCustomValidity("Please Enter your name")
+        }
+        if(!email.value.includes("@")){
+            email.setCustomValidity("Email must contain '@'")
+        }
+        if(password.value.length<6){
+            password.setCustomValidity("password must include more than 6 characters")
+        }else{
+            password.setCustomValidity("")
+        }
+        if(!form.checkValidity()){
+            event.preventDefault()
+            form.reportValidity()
+        }
+    })
+*/
+
+const  lists=document.getElementsByClassName("list")
+let rightBox=document.querySelector(".right")
+let leftBox=document.querySelector(".left")
+for(list of lists){
+    list.addEventListener("dragstart",function(e){
+        let selected=e.target
+  
+    rightBox.addEventListener("dragover",function(e){
+        e.preventDefault()
+    })
+    rightBox.addEventListener("drop",function(e){
+        rightBox.appendChild(selected)
+        selected=null
+    })
+    leftBox.addEventListener("dragover",function(e){
+        e.preventDefault()
+    })
+    leftBox.addEventListener("drop",function(e){
+        leftBox.appendChild(selected)
+        selected=null
+    })
+  })
+}
