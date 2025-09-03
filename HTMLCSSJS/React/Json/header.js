0a118f4d497a564a28c5856224799961df3dc1a5
Athulya <athulyaa175@gmail.com>
2025-09-03 15:12:34 +0530

    Json

--- HTMLCSSJS/React/Json/header.js
+++ HTMLCSSJS/React/Json/header.js
@@ -18,3 +18,24 @@ fetch("./header.json")
     })
     navbar.appendChild(navRight)
 })
+
+fetch("./Product.json")
+.then(response=>response.json())
+.then(data=>{
+    let container=document.querySelector(".container")
+    data.forEach(pdt=>{
+        let card =document.createElement("div")
+        card.className="card"
+        card.innerHTML=`
+       <div class="imgcnt"> <img src=${pdt.img} class="pdt-img"></div>
+        <div class="pdtdtls">
+            <div class="pdt-left">
+            <div class="pdtName">${pdt.name}</div>
+            <div class="Ratings"><span id="rdlts"><div class=</span></div>
+            </div>
+        </div>
+        `
+        container.appendChild(card)
+    })
+
+})
