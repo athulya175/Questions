7e9bf5e621ce1edb1b74e72f675a6123e1d69bf4
Athulya <athulyaa175@gmail.com>
2025-09-04 09:34:13 +0530

    FilterByRam

---
+++ HTMLCSSJS/React/Json/JSONW3/W3Notes.md
@@ -0,0 +1,18 @@
+* JSON stands for JavaScript Object Notation
+* JSON is a plain text format for storing and transporting data
+* JSON is similar to the syntax for creating JavaScript object
+* Json is used to send, receive and strore data.
+
+Why JSON?
+* JSON is language independent
+
+
+JSON & JavaScript
+-----------------
+
+JS has a built in function for converting JSON string to JS object.
+    JSON.Parse()
+JS has a built in function for converting object to JSON string
+    JSON.stringify(
+        
+    )
--- HTMLCSSJS/React/Json/header.js
+++ HTMLCSSJS/React/Json/header.js
@@ -1,3 +1,37 @@
+function generateLIstItems(pdt){
+    return`
+            <li class="detLi"> ${pdt.ram}GB RAM | ${pdt.rom} GB ROM | ${pdt.expandale&&"Expandable"}</li>
+        <li class="detLi">${pdt.display["size-cm"]}cm (${pdt.display["size-inch"]}inch) ${pdt.display.type}</li>
+        <li class="detLi">${pdt.camera.rear}|${pdt.camera.front} Front Camera</li>
+        <li class="detLi">${pdt.battery.capacity} ${pdt.battery.desc}</li>
+        <li class="detLi">${pdt.processor.brand} ${pdt.processor.model} ${pdt.processor.cores}-Core ${pdt.processor.speed}</li>
+        <li class="detLi">${pdt.warenty}</li>`
+    
+}
+
+
+function createCard(pdt){
+    let card=document.createElement("div")
+    card.className="card"
+    let listItems=generateLIstItems(pdt)
+     card.innerHTML=`
+       <div class="imgcnt"> <img src=${pdt.img} class="pdt-img"></div>
+        <div class="pdtdtls">
+            <div class="pdt-left">
+            <div class="pdtName">${pdt.name}</div>
+            <div class="Ratings"><span id="rdlts"><div class="ratcount">${pdt.rats}<img src=${pdt.ratimg} class="starimg"></div></span>
+                <span class="ratitle">${pdt.rating} Rattings & ${pdt.reviews} Reviews
+            </div>
+            <div class="details">
+            <ul class="unorder">
+            ${listItems}
+            </ul>
+            </div>
+            </div>
+        </div>
+        `
+        return card;
+}
 fetch("./header.json")
 .then(response=>response.json())
 .then(data=>{
@@ -24,97 +58,28 @@ fetch("./Product.json")
 .then(data=>{
     let container=document.querySelector(".container")
     data.forEach(pdt=>{
-        let card =document.createElement("div")
-        card.className="card"
-        let listItems=`
-        <li class="detLi"> ${pdt.ram}GB RAM | ${pdt.rom} GB ROM | ${pdt.expandale&&"Expandable"}</li>
-        <li class="detLi">${pdt.display["size-cm"]}cm (${pdt.display["size-inch"]}inch) ${pdt.display.type}</li>
-        <li class="detLi">${pdt.camera.rear}|${pdt.camera.front} Front Camera</li>
-        <li class="detLi">${pdt.battery.capacity} ${pdt.battery.desc}</li>
-        <li class="detLi">${pdt.processor.brand} ${pdt.processor.model} ${pdt.processor.cores}-Core ${pdt.processor.speed}</li>
-        <li class="detLi">${pdt.warenty}</li>`
-        card.innerHTML=`
-       <div class="imgcnt"> <img src=${pdt.img} class="pdt-img"></div>
-        <div class="pdtdtls">
-            <div class="pdt-left">
-            <div class="pdtName">${pdt.name}</div>
-            <div class="Ratings"><span id="rdlts"><div class="ratcount">${pdt.rats}<img src=${pdt.ratimg} class="starimg"></div></span>
-                <span class="ratitle">${pdt.rating} Rattings & ${pdt.reviews} Reviews
-            </div>
-            <div class="details">
-            <ul class="unorder">
-            ${listItems}
-            </ul>
-            </div>
-            </div>
-        </div>
-        `
+        let card=createCard(pdt)
         container.appendChild(card)
     })
 
 })
 
-/*
+
 fetch("./Product.json")
-.then(response=>response.json())
-.then(data=>{
-    let leftpage=document.querySelector(".left-page")
-    let brands=[...new Set(data.map(item=>item.brand))]
-    let rams=[...new Set(data.map(item=>item.ram))]
-    data.forEach(fil=>{
-        let filterLayout=document.createElement("div")
-        filterLayout.className="filterLayout"
-        filterLayout.innerHTML=`
-        <div class="filcontainer">
-        <section class="filTitle">Filter</section>
-        <section class="filSub">BRAND
-            <div class="brand1"></div>
-        </section>
-        <section class="filSub">
-        RAM<div id="ram1"></div>
-        </section>
-        </div>`
-        leftpage.appendChild(filterLayout)
-    })
-})
-*/
-fetch("./Product.json")
+
 .then(response => response.json())
+//Once the file is fetched,it converts the raw response into JSON format so you can use ut as JavaScript object/array
 .then(data => {
+    //After parsing,the "data" variable contains the full array of products from product.json
     let container = document.querySelector(".container");
     let filterBtns = document.querySelectorAll(".filter button");
 
     // function to render products
     function renderProducts(products){
-        container.innerHTML = ""; // clear old items
+        container.innerHTML = ""; 
+        // clear old items
         products.forEach(pdt => {
-            let card = document.createElement("div");
-            card.className = "card";
-
-            let listItems = `
-                <li class="detLi">${pdt.ram}GB RAM | ${pdt.rom} GB ROM | ${pdt.expandable && "Expandable"}</li>
-                <li class="detLi">${pdt.display["size-cm"]}cm (${pdt.display["size-inch"]} inch) ${pdt.display.type}</li>
-                <li class="detLi">${pdt.camera.rear} | ${pdt.camera.front} Front Camera</li>
-                <li class="detLi">${pdt.battery.capacity} ${pdt.battery.desc}</li>
-                <li class="detLi">${pdt.processor.brand} ${pdt.processor.model} ${pdt.processor.cores}-Core ${pdt.processor.speed}</li>
-                <li class="detLi">${pdt.warenty}</li>
-            `;
-
-            card.innerHTML = `
-                <div class="imgcnt"><img src=${pdt.img} class="pdt-img"></div>
-                <div class="pdtdtls">
-                    <div class="pdt-left">
-                        <div class="pdtName">${pdt.name}</div>
-                        <div class="Ratings">
-                            <span id="rdlts"><div class="ratcount">${pdt.rats}<img src=${pdt.ratimg} class="starimg"></div></span>
-                            <span class="ratitle">${pdt.rating} Ratings & ${pdt.reviews} Reviews</span>
-                        </div>
-                        <div class="details">
-                            <ul class="unorder">${listItems}</ul>
-                        </div>
-                    </div>
-                </div>
-            `;
+        let card=createCard(pdt)
             container.appendChild(card);
         });
     }
@@ -125,15 +90,22 @@ fetch("./Product.json")
     // add filter functionality
     filterBtns.forEach(btn => {
         btn.addEventListener("click", () => {
-            let ram = btn.getAttribute("data-ram");
-            if(ram === "all"){
+            let Ram = btn.getAttribute("data-ram");
+            //reads the data-ram attribute from the click button 
+            //ex:<button data-ram="4">4Gb</button>   -->ram=4
+            if(Ram === "all"){
                 renderProducts(data);
             } else {
-                let filtered = data.filter(p => p.ram == ram);
+                let filtered = data.filter(p => p.ram == Ram);
                 renderProducts(filtered);
+                //other wise filter data where product RAM matches the button CSSMathValue,then display only those
             }
         });
     });
 });
+
+
+
+
 
 
